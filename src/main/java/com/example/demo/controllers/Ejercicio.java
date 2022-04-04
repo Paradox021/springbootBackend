bf848d373f1f36030ac5327f6d1dee7188245ec3
Paradox021 <espanacarmelo@gmail.com>
2022-04-04 12:03:08 +0200

    funcion suma

--- src/main/java/com/example/demo/controllers/Ejercicio.java
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
+import java.text.MessageFormat;
+
 import com.example.demo.utils.Utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +29,13 @@ public class Ejercicio {
     public String palindrome(@PathVariable String name){
         boolean palindrome = Utils.isPalindrome(name);
         return palindrome ? "Si es palindromo":"No es palindromo";
+    }
+
+    // http://localhost:8080/sumar?num1=5&num2=10
+    @GetMapping("/sumar")
+    public String add(@RequestParam String num1, @RequestParam String num2){
+        int resultado = Integer.parseInt(num1) + Integer.parseInt(num2);
+        Object params[] = {num1, num2, resultado};
+        return MessageFormat.format("La suma de {0} y {1} es {2} " , params);
     }
 }
