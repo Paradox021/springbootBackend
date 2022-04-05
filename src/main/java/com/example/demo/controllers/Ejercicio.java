2983633a97380d6b940a7e3dad8b9e8c052f3273
Paradox021 <espanacarmelo@gmail.com>
2022-04-05 11:46:28 +0200

    guardar en disco

--- src/main/java/com/example/demo/controllers/Ejercicio.java
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -1,10 +1,15 @@
 package com.example.demo.controllers;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.MessageFormat;
+import java.util.Map;
 
 import com.example.demo.utils.Utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,4 +43,30 @@ public class Ejercicio {
         Object params[] = {num1, num2, resultado};
         return MessageFormat.format("La suma de {0} y {1} es {2} " , params);
     }
+
+    
+    @PostMapping("/saveProductOnDisk")
+    public String saveProduct(@RequestParam Map<String,String> body){
+        String ArticleValue = body.get("article");
+        String priceValue = body.get("price");
+        
+        //valido si no son elementos vacios
+        if(ArticleValue.equals("") || priceValue.equals("")){
+            return "Error,datos incorrectos";
+        }
+        // precio negativo
+        if(Integer.valueOf(priceValue) < 0){
+            return "Precio negativo";
+        }
+        
+        try {
+            Utils.save("datos.txt", ArticleValue+","+priceValue+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error al escribir en disco";
+        }
+       // Devolver msg cliente
+       return "Producto guardado correctamente";
+       
+    }   
 }
--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -1,10 +1,26 @@
 package com.example.demo.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Utils {
     public static boolean isPalindrome(String value){
         value = value.replace(" ", "");
         StringBuilder builder =new StringBuilder(value);
         String reversedName = builder.reverse().toString();
         return value.equals(reversedName);
+    }
+
+    public static void save(String fileName, String text) throws IOException{
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter(fileName, true);
+            pw = new PrintWriter(fw);
+            pw.print(text);
+        } finally{
+            if(pw!=null)pw.close();
+        }
     }
 }
