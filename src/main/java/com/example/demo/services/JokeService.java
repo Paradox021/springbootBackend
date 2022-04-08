ff7109e2d36f461aada90c1bd85020dae87ad36d
paradox021 <espanacarmelo@gmail.com>
2022-04-08 13:09:05 +0200

    db

---
+++ src/main/java/com/example/demo/services/JokeService.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import java.util.ArrayList;
+
+import com.example.demo.models.Joke;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeService {
+    public void saveJoke(){
+        
+    }
+
+    public ArrayList<Joke> getAllJokes(){
+        return null;
+    }
+}
